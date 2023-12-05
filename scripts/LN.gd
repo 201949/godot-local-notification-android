516c731f913e4a581a525a099a08da2c535660e8
Mike <70590729+201949@users.noreply.github.com>
2023-12-05 20:27:13 +0300

    Update LN.gd

--- scripts/LN.gd
+++ scripts/LN.gd
@@ -18,25 +18,22 @@ onready var _analytics := $'/root/analytics' if has_node('/root/analytics') else
 #1 Add the plugin itself to the plugins folder.
 #2 Enable plugin in export
 #3 Give the plugin the necessary permissions (in the export menu) - android.permission.SCHEDULE_EXACT_ALARM android.permission.USE_EXACT_ALARM android.permission.POST_NOTIFICATIONS
-#4 Add all the necessary icons to the mipmap (folder) small icon 96x96 large 256x256
+#4 Add all the necessary icons to the mipmap (folder) small icon 96x96 (B/W), large 256x256 (RGB)
 
-
-var once_message = "Once Notify Message" #Just Once notification title text 
-var repeat_message = "Repeat Notify Message" #Just Repeat notification title text 
 #The text of your notification. 
 #For a more aesthetically pleasing notification, I disabled the long text.
 #If you need it, write me, I will do it.
 
-
-var once_title = "Once Notify Title" #Once Notification title
-
-var repeat_title = "Repeat Notify Title" #Repeat Notification title
-
-var small_icon_name = "small_icon" #The name of the small icon. You can put any name of your icon that is in the mipmap folder
-
-var large_icon_name = "notify_big" #the same as with the small icon
-
-var summary_text = "Notify Summary" #disabled for now, but must be passed when calling the function
+#var once_message = "Once Notify Message" #Just Once notification title text 
+#var repeat_message = "Repeat Notify Message" #Just Repeat notification title text 
+#var once_title = "Once Notify Title" #Once Notification title
+#var repeat_title = "Repeat Notify Title" #Repeat Notification title
+#var small_icon_name = "small_icon" #The name of the small icon. You can put any name of your icon that is in the mipmap folder
+#var large_icon_name = "notify_big" #the name as the big icon
+#var summary_text = "Notify Summary" #disabled for now, but must be passed when calling the function
+#var interval = 10 #Initial delay (seconds)
+#var repeat = 60 #Repetition rate (seconds)
+#var tag = 1 #Tag of the message - must be unique for multiply messages
 
 
 func _ready():
@@ -49,15 +46,13 @@ func init() -> void:
 	if _ln != null:
 		_ln.init()
 
-func show_once(): #One-time notification
+func show_once(once_message, once_title, interval, tag, small_icon_name, large_icon_name, summary_text, color): #One-time notification
 	if _ln != null:
-		var color = "#ffff00"
-		_ln.showLocalNotification(once_message, once_title, 8, 1, small_icon_name, large_icon_name, summary_text, color)
+		_ln.showLocalNotification(once_message, once_title, interval, tag, small_icon_name, large_icon_name, summary_text, color)
 
-func show_repeat(): #Repeat notification
+func show_repeat(repeat_message, repeat_title , interval, tag, repeat, small_icon_name, large_icon_name, summary_text, color): #Repeat notification
 	if _ln != null:
-		var color = "#ff00ff"
-		_ln.showRepeatingNotification(repeat_message, repeat_title , 60, 2, 60, small_icon_name, large_icon_name, summary_text, color)
+		_ln.showRepeatingNotification(repeat_message, repeat_title , interval, tag, repeat, small_icon_name, large_icon_name, summary_text, color)
 
 func _cancel_notification(tag):
 	if _ln != null:
